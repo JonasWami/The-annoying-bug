36234a052472c680e7ca3674ebbe9c16b31711db
JonasWami <92530759+JonasWami@users.noreply.github.com>
2021-11-02 20:43:03 +0100

    added freezing of physics rotations

--- The annoying bug/Assets/Scripts/Fly_movment.cs
+++ The annoying bug/Assets/Scripts/Fly_movment.cs
@@ -17,27 +17,50 @@ public class Fly_movment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Forward velocity
          if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.right * -ForwardSpeed * Time.deltaTime);
 
         }
+         //Rotate Right
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(Vector3.up * LeftAndRightSpeed * Time.deltaTime);
+
+            // Freeze physics rotation
+            rb.freezeRotation = true;
         }
+        //Rotate left
        if(Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.up * -LeftAndRightSpeed * Time.deltaTime);
+
+            // Freeze physics rotation
+            rb.freezeRotation = true;
         
         }
+       //Rotate up
       else  if(Input.GetKey(KeyCode.W))
         {
             transform.Rotate(Vector3.forward * -UpAndDownSpeed * Time.deltaTime);
+
+            // Freeze physics rotation
+            rb.freezeRotation = true;
         }
+       // rotate down
       else  if(Input.GetKey(KeyCode.S))
         {
             transform.Rotate(Vector3.forward * UpAndDownSpeed * Time.deltaTime);
+
+            // Freeze physics rotation
+            rb.freezeRotation = true;
+        }
+
+       else
+        {
+            //Start physics rotation
+            rb.freezeRotation = false;
         }
         
       
